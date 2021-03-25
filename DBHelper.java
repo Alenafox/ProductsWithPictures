e32b4e2632e20c4a7bc71bb5055acb41f8d26ce3
Alenafox <57696470+Alenafox@users.noreply.github.com>
2021-03-25 13:39:22 +0800

    Create DBHelper.java

---
+++ DBHelper.java
@@ -0,0 +1,33 @@
+package com.example.productswithpictures;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    final static String DB_NAME = "products.db";
+    final static String TABLE_NAME = "products";
+    final static String CREATE = "CREATE TABLE "+TABLE_NAME+ "( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL, `category` TEXT NOT NULL, `price` INTEGER NOT NULL, `picture` TEXT NOT NULL)";
+    // при изменении структуры БД меняется и версия
+    private static final int DATABASE_VERSION = 10;
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // выполняется, если базы данных нет
+        db.execSQL(CREATE);
+        db.execSQL("INSERT INTO products VALUES (1, 'pion','garden flowers', 300, 'https://centre-flower.ru/wp-content/uploads/p/2/9/7/4/2974-Pion-Sara-Bernar.jpg' )," +
+                "(2, 'rose','garden flowers', 600, 'https://pocvetam.ru/wp-content/uploads/2020/08/1-roza-1.jpg' )");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // выполняется, если версия базы данных отличается
+        db.execSQL("DROP DATABASE "+DB_NAME);
+        this.onCreate(db);
+    }
+}
